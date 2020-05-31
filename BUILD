836b54e3d49060746ab987170575b989350b42c3
Philipp Wollermann <philipp.wollermann@gmail.com>
2020-05-31 11:28:08 +0200

    Check in basic project structure.

---
+++ BUILD
@@ -0,0 +1,5 @@
+java_binary(
+    name = "helloworld",
+    srcs = glob(["src/main/java/de/philwo/helloworld/*.java"]),
+    main_class = "de.philwo.helloworld.Main",
+)
---
+++ src/main/java/de/philwo/helloworld/Main.java
@@ -0,0 +1,7 @@
+package de.philwo.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
